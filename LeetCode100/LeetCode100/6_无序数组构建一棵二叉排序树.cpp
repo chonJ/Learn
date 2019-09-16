c1117c67dcd8bbaa46543d551d5f8b5b19795de2
chonJ <jichong@bupt.edu.cn>
2019-09-16 16:46:28 +0800

    gengxi

---
+++ LeetCode100/LeetCode100/6_无序数组构建一棵二叉排序树.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+void funcCore(vector<int> nums, int i, TreeNode* root)
+{
+	if (i == nums.size())
+		return;
+	
+	else if (nums[i] < root->val)
+	{
+		if (root->left == NULL)
+			root->left = new TreeNode(nums[i]);
+		else
+			funcCore(nums, i, root->left);
+
+	}
+	else
+	{
+		if (root->right == NULL)
+			root->right = new TreeNode(nums[i]);
+		else
+			funcCore(nums, i, root->right);
+	}
+	
+}
+
+TreeNode* func(vector<int> nums)
+{
+	if (nums.size() == 0)
+		return NULL;
+	TreeNode* root = new TreeNode(nums[0]);
+	for(int i = 1;i < nums.size();++i)
+		funcCore(nums, i, root);
+
+	
+	return root;
+}
+
+
+void print(TreeNode* root)
+{
+	if (root == NULL)
+		return;
+	cout << root->val << endl;
+	print(root->left);
+	print(root->right);
+}
+int main()
+{
+	vector<int> nums = { 10,8,9,6,12,11,13 };
+	TreeNode* root = func(nums);
+	print(root);
+	system("pause");
+	return 0;
+}
